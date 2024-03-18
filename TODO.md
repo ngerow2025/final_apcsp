cd5ad590fe8a3467058124ba7bc9ef3cf094ba11
ngerow2025 <ngerow12025@gmail.com>
2024-03-18 10:57:45 -0400

    TODO and start of tokenizer
    from 3/14

---
+++ TODO.md
@@ -0,0 +1,9 @@
+# needed components
+
+- [x] output
+- [x] input
+- [ ] list
+- [ ] procedure
+- [ ] loop/algorithm
+- [x] function call
+- [ ] boolean with if
--- src/main.rs
+++ src/main.rs
@@ -22,6 +22,44 @@ fn main_loop() {
 }
 
 fn solve(input: &str) -> f64 {
+    let tokens = tokenize(input);
     println!("Solving: {}", input);
     0.0
+}
+
+enum Token {
+    Number(f64),
+    Plus, Minus, Multiply, Divide,
+    OpenParen, CloseParen,
+    Variable(String),
+    Sin, Cos, Tan,
+    Csc, Sec, Cot,
+    Arcsin, Arccos, Arctan,
+    Arccsc, Arcsec, Arccot,
+    Pow,
+    Sqrt,
+    Pi,
+}
+
+
+
+fn tokenize(input: &str) -> Vec<Token> {
+    let mut tokens = Vec::new();
+    let mut iter = input.chars().peekable();
+    while iter.peek() != None {
+        let c = iter.peek().unwrap();
+        if c.is_ascii_digit() {
+            let mut number = String::new();
+            while iter.peek().unwrap().is_ascii_digit() || iter.peek().unwrap() == &'.'{
+                number.push(iter.next().unwrap());
+            }
+            tokens.push(Token::Number(number.parse().unwrap()));
+            continue;
+        }
+
+
+
+
+    }
+    tokens
 }
