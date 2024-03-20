772c3f54544d73b85444c32a493a36a1fe1c5504
ngerow2025 <ngerow12025@gmail.com>
2024-03-19 21:32:54 -0400

    Expression type + started coalesce
    - wrote Expression type and all supporting types to help with simplification
    - outlined the coalesce function with basic conversions
    - wrote the actual coalescing for addition

--- src/main.rs
+++ src/main.rs
@@ -23,10 +23,9 @@ fn main_loop() {
 fn solve(input: String) -> f64 {
     let tokens = tokenize(input);
     let ast = parse(tokens);
+    let simplified = coalesce(&ast[0]);
     println!("Solving:");
-    for node in &ast {
-        print_ast(node, 0);
-    }
+    print_expression(&simplified, 0);
     0.0
 }
 
@@ -456,4 +455,237 @@ fn parse(tokens: Vec<Token>) -> Vec<ASTNode> {
     }
     true_output.push(expression_output.pop_back().unwrap());
     true_output
+}
+
+struct Multiplication {
+    terms: Vec<Box<Expression>>,
+}
+
+struct Division {
+    numerator: Box<Expression>,
+    denominator: Box<Expression>,
+}
+
+struct Addition {
+    terms: Vec<Box<Expression>>,
+}
+
+struct Negation {
+    term: Box<Expression>,
+}
+
+struct Exponentiation {
+    base: Box<Expression>,
+    exponent: Box<Expression>,
+}
+
+struct Sqrt {
+    arg: Box<Expression>,
+}
+
+enum Function {
+    Sin(Box<Expression>),
+    Cos(Box<Expression>),
+    Tan(Box<Expression>),
+    Csc(Box<Expression>),
+    Sec(Box<Expression>),
+    Cot(Box<Expression>),
+    Arcsin(Box<Expression>),
+    Arccos(Box<Expression>),
+    Arctan(Box<Expression>),
+    Arccsc(Box<Expression>),
+    Arcsec(Box<Expression>),
+    Arccot(Box<Expression>),
+}
+enum Expression {
+    Number(f64),
+    Variable(String),
+    Multiplication(Multiplication),
+    Division(Division),
+    Addition(Addition),
+    Negation(Negation),
+    Exponentiation(Exponentiation),
+    Sqrt(Sqrt),
+    Function(Function),
+}
+
+fn coalesce(ast: &ASTNode) -> Expression {
+    match ast {
+        ASTNode::Number(n) => Expression::Number(*n),
+        ASTNode::PI => Expression::Number(std::f64::consts::PI),
+        ASTNode::Variable(v) => Expression::Variable(v.clone()),
+        ASTNode::BinaryOp(lhs, rhs, op) => {
+            match op {
+                BinaryOp::Add => {
+                    let lhs = coalesce(lhs);
+                    let rhs = coalesce(rhs);
+                    let mut terms = vec![Box::new(lhs), Box::new(rhs)];
+
+                    let mut simplified = false;
+                    while !simplified {
+                        simplified = true;
+                        terms = terms.into_iter().flat_map(|term| {
+                            match *term {
+                                Expression::Addition(addition) => {
+                                    simplified = false;
+                                    addition.terms.into_iter()
+                                },
+                                _ => vec![term].into_iter(),
+                            }
+                        }).collect();
+                    }
+
+                    Expression::Addition(Addition {
+                        terms,
+                    })
+                },
+                BinaryOp::Subtract => {
+                    let lhs = coalesce(lhs);
+                    let rhs = coalesce(rhs);
+                    Expression::Addition(Addition {
+                        terms: vec![Box::new(lhs), Box::new(Expression::Negation(Negation {
+                            term: Box::new(rhs),
+                        }))],
+                    })
+                },
+                BinaryOp::Multiply => {
+                    let lhs = coalesce(lhs);
+                    let rhs = coalesce(rhs);
+                    Expression::Multiplication(Multiplication {
+                        terms: vec![Box::new(lhs), Box::new(rhs)],
+                    })
+                },
+                BinaryOp::Divide => {
+                    let lhs = coalesce(lhs);
+                    let rhs = coalesce(rhs);
+                    Expression::Division(Division {
+                        numerator: Box::new(lhs),
+                        denominator: Box::new(rhs),
+                    })
+                },
+                BinaryOp::Pow => {
+                    let base = coalesce(lhs);
+                    let exponent = coalesce(rhs);
+                    Expression::Exponentiation(Exponentiation {
+                        base: Box::new(base),
+                        exponent: Box::new(exponent),
+                    })
+                },
+            }
+        },
+        ASTNode::UnaryOp(arg, op) => {
+            let arg = coalesce(arg);
+            match op {
+                UnaryOp::Sin => Expression::Function(Function::Sin(Box::new(arg))),
+                UnaryOp::Cos => Expression::Function(Function::Cos(Box::new(arg))),
+                UnaryOp::Tan => Expression::Function(Function::Tan(Box::new(arg))),
+                UnaryOp::Csc => Expression::Function(Function::Csc(Box::new(arg))),
+                UnaryOp::Sec => Expression::Function(Function::Sec(Box::new(arg))),
+                UnaryOp::Cot => Expression::Function(Function::Cot(Box::new(arg))),
+                UnaryOp::Arcsin => Expression::Function(Function::Arcsin(Box::new(arg))),
+                UnaryOp::Arccos => Expression::Function(Function::Arccos(Box::new(arg))),
+                UnaryOp::Arctan => Expression::Function(Function::Arctan(Box::new(arg))),
+                UnaryOp::Arccsc => Expression::Function(Function::Arccsc(Box::new(arg))),
+                UnaryOp::Arcsec => Expression::Function(Function::Arcsec(Box::new(arg))),
+                UnaryOp::Arccot => Expression::Function(Function::Arccot(Box::new(arg))),
+                UnaryOp::Sqrt => Expression::Sqrt(Sqrt {
+                    arg: Box::new(arg),
+                }),
+            }
+        },
+    }
+}
+
+fn print_expression(expression: &Expression, indent: i32) {
+    print!("{}", "| ".repeat(indent as usize));
+    match expression {
+        Expression::Number(n) => {
+            println!("Number: {}", n);
+        },
+        Expression::Variable(v) => {
+            println!("Variable: {}", v);
+        },
+        Expression::Multiplication(multiplication) => {
+            println!("Multiplication");
+            for term in &multiplication.terms {
+                print_expression(term, indent + 1);
+            }
+        },
+        Expression::Division(division) => {
+            println!("Division");
+            print_expression(&division.numerator, indent + 1);
+            print_expression(&division.denominator, indent + 1);
+        },
+        Expression::Addition(addition) => {
+            println!("Addition");
+            for term in &addition.terms {
+                print_expression(term, indent + 1);
+            }
+        },
+        Expression::Negation(negation) => {
+            println!("Negation");
+            print_expression(&negation.term, indent + 1);
+        },
+        Expression::Exponentiation(exponentiation) => {
+            println!("Exponentiation");
+            print_expression(&exponentiation.base, indent + 1);
+            print_expression(&exponentiation.exponent, indent + 1);
+        },
+        Expression::Sqrt(sqrt) => {
+            println!("Sqrt");
+            print_expression(&sqrt.arg, indent + 1);
+        },
+        Expression::Function(function) => {
+            match function {
+                Function::Sin(arg) => {
+                    println!("Sin");
+                    print_expression(arg, indent + 1);
+                },
+                Function::Cos(arg) => {
+                    println!("Cos");
+                    print_expression(arg, indent + 1);
+                },
+                Function::Tan(arg) => {
+                    println!("Tan");
+                    print_expression(arg, indent + 1);
+                },
+                Function::Csc(arg) => {
+                    println!("Csc");
+                    print_expression(arg, indent + 1);
+                },
+                Function::Sec(arg) => {
+                    println!("Sec");
+                    print_expression(arg, indent + 1);
+                },
+                Function::Cot(arg) => {
+                    println!("Cot");
+                    print_expression(arg, indent + 1);
+                },
+                Function::Arcsin(arg) => {
+                    println!("Arcsin");
+                    print_expression(arg, indent + 1);
+                },
+                Function::Arccos(arg) => {
+                    println!("Arccos");
+                    print_expression(arg, indent + 1);
+                },
+                Function::Arctan(arg) => {
+                    println!("Arctan");
+                    print_expression(arg, indent + 1);
+                },
+                Function::Arccsc(arg) => {
+                    println!("Arccsc");
+                    print_expression(arg, indent + 1);
+                },
+                Function::Arcsec(arg) => {
+                    println!("Arcsec");
+                    print_expression(arg, indent + 1);
+                },
+                Function::Arccot(arg) => {
+                    println!("Arccot");
+                    print_expression(arg, indent + 1);
+                },
+            }
+        },
+    }
 }
